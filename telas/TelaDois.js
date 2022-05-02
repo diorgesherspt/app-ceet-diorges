e6bf94a896572294817e6479414dc968678419d7
diorgesherspt <diorgesherspt@outlook.com.br>
2022-05-02 09:09:00 -0300

    versão teste

---
+++ telas/TelaDois.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function TelaDois() {
+  return (
+    <View style={{padding:10}}>
+      <Text style={{textAlign:'justify'}}>Teste</Text>
+    </View>
+  );
+}
